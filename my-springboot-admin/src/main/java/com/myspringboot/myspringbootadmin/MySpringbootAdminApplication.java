ff5ba0480664e9014171efef779484cfac9e80f6
test <town2008@163.com>
2024-03-19 14:23:54 +0800

    admin相关

---
+++ my-springboot-admin/src/main/java/com/myspringboot/myspringbootadmin/MySpringbootAdminApplication.java
@@ -0,0 +1,15 @@
+package com.myspringboot.myspringbootadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class MySpringbootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringbootAdminApplication.class, args);
+	}
+
+}
---
+++ my-springboot-admin/src/test/java/com/myspringboot/myspringbootadmin/MySpringbootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspringboot.myspringbootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringbootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
