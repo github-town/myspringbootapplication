cf5f2aae9b6e8f77b9ddba905d90e8cdaae9c28c
zhoutao123 <fishcome2008@163.com>
2024-08-18 16:43:16 +0800

    NettyServer实现测试

---
+++ my-springboot-first-application/src/test/java/com/myspringboot/myspringbootfirstapplication/service/NettyServerTest.java
@@ -0,0 +1,54 @@
+package com.myspringboot.myspringbootfirstapplication.service;
+
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class NettyServerTest {
+    private final int port;
+
+    public NettyServerTest(int port) {
+        this.port = port;
+    }
+
+    public void start() throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new StringDecoder());
+                            ch.pipeline().addLast(new StringEncoder());
+                            ch.pipeline().addLast(new SimpleServerHandler());
+                        }
+                    });
+
+            ChannelFuture f = b.bind(port).sync();
+            System.out.println("Server started on port: " + port);
+            f.channel().closeFuture().sync();
+            System.out.println("f.channel().closeFuture().sync()");
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int port = 8080; // 可以根据需要更改端口
+        new NettyServerTest(port).start();
+    }
+}
---
+++ my-springboot-first-application/src/test/java/com/myspringboot/myspringbootfirstapplication/service/SimpleServerHandler.java
@@ -0,0 +1,26 @@
+package com.myspringboot.myspringbootfirstapplication.service;
+
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SimpleServerHandler extends SimpleChannelInboundHandler<String> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) {
+        log.info("Received message: " + msg);
+        StringBuilder stringBuilder = new StringBuilder("HTTP/1.1 200 OK\r\n");
+        stringBuilder.append("Content-Type:application/json\r\n");
+        stringBuilder.append("\r\n");
+        stringBuilder.append("success");
+        stringBuilder.append("你好，客户端！你发送的数据是: " + msg);
+        ctx.writeAndFlush(stringBuilder.toString());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
